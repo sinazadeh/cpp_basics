a12bb6e32eaeef67f4ed31f7c977c3a684a06831
Sina <luciferian11@gmail.com>
2021-01-24 15:49:10 +0330

    Change code style

--- cpp_random_number_generator/function.cpp
+++ cpp_random_number_generator/function.cpp
@@ -4,14 +4,16 @@
 unsigned int random_number(unsigned int min, unsigned int max){
   // Not included
   unsigned int answer;
+  
   srand(time(NULL));
   if (min == 0 && max == 0){
     answer = rand();
   }
   else{
-    answer = rand()%max;
     unsigned int tries = 0;
     unsigned int maxtries = 50;
+
+    answer = rand()%max;
     while (answer < min + 1 && tries < maxtries){
       answer = rand()%max;
       tries ++;
