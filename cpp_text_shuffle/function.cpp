8adcb12dc2567e67b863f6d9af7fa4d475c6ec74
Sina <luciferian11@gmail.com>
2021-01-28 09:07:40 +0330

    Improve function

--- cpp_text_shuffle/function.cpp
+++ cpp_text_shuffle/function.cpp
@@ -6,7 +6,7 @@ using std::cout;
 
 string text_shuffle(string text){
   srand(time(NULL));
-  for (uintmax_t i=0; i<(text.size())/2; i++){
+  for (uintmax_t i=0; i<(text.size()/2)+1; i++){
     uintmax_t random_index = rand()%text.size();
     text.push_back(text[random_index]);
     text.erase(random_index, 1);
