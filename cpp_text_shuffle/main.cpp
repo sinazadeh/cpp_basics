804e75d1a3b696cccc3474b8bc01d0e4510dff17
Sina <luciferian11@gmail.com>
2021-01-28 09:33:01 +0330

    Add another example

--- cpp_text_shuffle/main.cpp
+++ cpp_text_shuffle/main.cpp
@@ -6,5 +6,8 @@ using std::string;
 int main(){
   string text = "darling";
   string shuffled_text = text_shuffle(text);
-  cout << shuffled_text;
+  cout << shuffled_text << "\n\n";
+  string long_text = "Git (/ɡɪt/) is a distributed version-control system for tracking changes in any set of files, originally designed for coordinating work among programmers cooperating on source code during software development. Its goals include speed, data integrity, and support for distributed, non-linear workflows.";
+  string shuffled_long_text = text_shuffle(long_text);
+  cout << shuffled_long_text;
 }
